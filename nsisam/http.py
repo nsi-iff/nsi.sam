c8749b1031100f9faad477e3fdb91177081c1de5
Douglas Camata <d.camata@gmail.com>
2011-08-06 00:31:09 -0300

    servidor web do sam refatorado para usar sharding

--- nsisam/http.py
+++ nsisam/http.py
@@ -37,13 +37,12 @@ class HttpHandler(cyclone.web.RequestHandler):
     @cyclone.web.asynchronous
     def get(self):
         self._check_auth()
-        self.set_header('Content-Type', 'application/json')
-        for db in iter(self.settings.db_list):
-            key = self._load_request_as_json().get('key')
-            value = yield db.get(key)
-            if value:
-                value = eval(value)
-                self.finish(cyclone.escape.json_encode(value))
+        key = self._load_request_as_json().get('key')
+        value = yield self.settings.db.get(key)
+        if value:
+            value = eval(value)
+            self.set_header('Content-Type', 'application/json')
+            self.finish(cyclone.escape.json_encode(value))
         else:
             raise cyclone.web.HTTPError(404, "Key not found.")
 
@@ -53,12 +52,11 @@ class HttpHandler(cyclone.web.RequestHandler):
         self._check_auth()
         self.set_header('Content-Type', 'application/json')
         key = str(uuid4())
-        db = choice(self.settings.db_list)
         today = datetime.today().strftime("%d/%m/%y %H:%M")
         user = self._get_current_user()[0]
         value = self._load_request_as_json().get('value')
         data_dict = {"data":value, "size":len(value), "date":today, "from_user": user}
-        result = yield db.set(key, data_dict)
+        result = yield self.settings.db.set(key, data_dict)
         checksum = self._calculate_sha1_checksum(dumps(data_dict))
         self.finish(cyclone.escape.json_encode({"key":key, "checksum":checksum}))
 
@@ -66,33 +64,30 @@ class HttpHandler(cyclone.web.RequestHandler):
     @cyclone.web.asynchronous
     def post(self):
         self._check_auth()
-        self.set_header('Content-Type', 'application/json')
         json_args = self._load_request_as_json()
         key = json_args.get('key')
-        for db in self.settings.db_list:
-            exists = yield db.exists(key)
-            if exists:
-                value = json_args.get('value')
-                today = datetime.today().strftime("%d/%m/%y %H:%M")
-                user = self._get_current_user()[0]
-                data_dict = {"data":value, "size":len(value), "date":today, "from_user": user}
-                result = yield db.set(key, data_dict)
-                checksum = self._calculate_sha1_checksum(dumps(data_dict))
-                self.finish(cyclone.escape.json_encode({'key':key, 'checksum':checksum}))
-            else:
-                raise cyclone.web.HTTPError(404, "Key not found.")
+        exists = yield self.settings.db.exists(key)
+        if exists:
+            value = json_args.get('value')
+            today = datetime.today().strftime("%d/%m/%y %H:%M")
+            user = self._get_current_user()[0]
+            data_dict = {"data":value, "size":len(value), "date":today, "from_user": user}
+            result = yield self.settings.db.set(key, data_dict)
+            checksum = self._calculate_sha1_checksum(dumps(data_dict))
+            self.set_header('Content-Type', 'application/json')
+            self.finish(cyclone.escape.json_encode({'key':key, 'checksum':checksum}))
+        else:
+            raise cyclone.web.HTTPError(404, "Key not found.")
 
     @defer.inlineCallbacks
     @cyclone.web.asynchronous
     def delete(self):
         self._check_auth()
-        self.set_header('Content-Type', 'application/json')
-        for db in self.settings.db_list:
-            key = self._load_request_as_json().get('key')
-            exists = yield db.exists(key)
-            if exists:
-              result = yield db.delete(key)
-              if result:
-                  self.finish(cyclone.escape.json_encode({'deleted':True}))
-        self.finish(cyclone.escape.json_encode({'deleted':False}))
+        key = yield self._load_request_as_json().get('key')
+        exists = yield self.settings.db.exists(key)
+        if exists and self.settings.db.delete(key):
+            self.set_header('Content-Type', 'application/json')
+            self.finish(cyclone.escape.json_encode({'deleted':True}))
+        else:
+            raise cyclone.web.HTTPError(404, "Key not found.")
 
