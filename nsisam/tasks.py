31504a0da2ccb86423a829ebba9e9e58ecc09236
Douglas Camata <d.camata@gmail.com>
2012-11-01 17:36:06 -0200

    task to clean files

--- nsisam/tasks.py
+++ nsisam/tasks.py
@@ -1,16 +1,17 @@
 from os import listdir, remove
-from celery import Celery, Task
-from redis import Redis
+from os.path import join
+from celery import Celery, Task, registry
+from restfulie import Restfulie
 
 celery =  Celery()
 celery.config_from_object('celeryconfig')
 
-class CleanFiles(Task):
-
-	def run(self, path, redis_config):
-		for server in redis_config:
-			redis = Redis(*server)
-			for entry in listdir(path):
-				if not redis.get(entry):
-					os.remove(entry)
-
+@celery.task
+def clean_files(path, sam_host, sam_port, sam_user, sam_password):
+    print 'Cleaning the files...'
+    sam = Restfulie.at("http://%s:%s/" % (sam_host, sam_port))
+    sam = sam.auth(sam_user, sam_password).as_('application/json')
+    for entry in listdir(path):
+        if sam.get(key=entry).code == '404':
+            remove(join(path, entry))
+    print 'Done.'
